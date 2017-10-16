7ffd8270dec80725af6b126fb7d91412e09d35a3
x373241884y <x373241884y@gmail.com>
2017-10-16 11:45:03 +0800

    okok

--- src/js/background.js
+++
@@ -1,6 +0,0 @@
-chrome.tabs.onCreated.addListener(function (tab) {
-	debugger;
-});
-chrome.tabs.onUpdated.addListener(function (tab) {
-	console.warn('tabsupdate');
-});
--- src/js/options.js
+++ src/js/options.js
@@ -1,29 +1,18 @@
 /**
  * Created by toor on 17-10-13.
  */
-// var city = localStorage.city || 'beijing';
-// document.getElementById('city').value = city;
-// document.getElementById('save').onclick = function(){
-// 	localStorage.city = document.getElementById('city').value;
-// 	alert('保存成功。');
-// }
 (function () {
-	var defaultCss = '\n\
-.console-error-level .console-message-text{\n\
-	color: red;\n\
-}\n\
-\n\
-.console-warning-level .console-message-text {\n\
-	color: orange;\n\
-}\n\
-\n\
-.console-log-level .console-message-text {\n\
-	color:green;\n\
-}\n';
-
-	var ctrl = document.getElementById('style-ctrl');
-	ctrl.value = defaultCss;
-	document.getElementById('save').addEventListener('click', function (event) {
-		console.log(ctrl.value);
-	});
+	var userStyle = localStorage.userStyle,styleText;
+	if(!userStyle){
+		var xhr = new XMLHttpRequest(),
+			stylesheet = 'css/default.css';
+		xhr.open("GET", "/" + stylesheet, false);
+		xhr.send();
+		styleText = xhr.responseText;
+		var ctrl = document.getElementById('style-ctrl');
+		ctrl.value = styleText;
+		document.getElementById('save').addEventListener('click', function (event) {
+			console.log(ctrl.value);
+		});
+	}
 }());
